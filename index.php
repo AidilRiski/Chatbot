a87c0275c35f6deb1fb9a0e12c86d7546ff455a8
AidilRiski <riskisultan@yahoo.com>
2019-04-13 03:38:14 +0700

    Test PHP.

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chatbot</title>
+    </head>
+    <body>
+        <h1>Welcome</h1>
+        <?php 
+            echo 'Chatbot';
+        ?>
+    </body>
+</html>

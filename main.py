63547eba2e82bb5b54f31fa2692df4240bba2678
Aidil Riski <riskisultan@yahoo.com>
2019-04-13 07:57:07 +0700

    Layout.

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+#Input: Sebuah string _pattern yang akan dicari pada sebuah string _stringToCheck.
+#Output: Persentasi kemiripan maksimal. (Jumlah karakter yang sama berurutan dibagi panjang _stringToCheck).
+#Memakai metode KMP
+def stringMatchKMP(_pattern, _stringToCheck):
+    similarityPercentage = 0
+    return similarityPercentage
+
+#Input: Sebuah string _pattern yang akan dicari pada sebuah string _stringToCheck.
+#Output: Persentasi kemiripan maksimal. (Jumlah karakter yang sama berurutan dibagi panjang _stringToCheck).
+#Memakai metode Boyer-Moore
+def stringMatchBoyerMoore(_pattern, _stringToCheck):
+    similarityPercentage = 0
+    return similarityPercentage
+
+#Input: Sebuah string _pattern yang akan dicari pada sebuah string _stringToCheck.
+#Output: Persentasi kemiripan maksimal. (Jumlah karakter yang sama berurutan dibagi panjang _stringToCheck).
+#Memakai Regular Expression
+def stringMatchRegex(_pattern, _stringToCheck):
+    similarityPercentage = 0
+    return similarityPercentage
+
 def main():
     print('Hello World')
 
